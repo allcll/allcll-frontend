2fd33f00d027cb9ac4532c552df4c934efd83f65
hyunwoo0081 <hyunwoo0081@gmail.com>
2025-02-08 18:05:40 +0900

    feat: setting ga & sentry

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,6 +4,8 @@ import {RouterProvider} from 'react-router-dom';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import ReactGA from "react-ga4";
+import * as Sentry from "@sentry/react";
 import { server } from "@allcll/mock-server";
 import router from '@/utils/routing.tsx';
 import './index.css'
@@ -12,15 +14,34 @@ const queryClient = new QueryClient();
 const UsingMockServer = false;
 const isProduction = process.env.NODE_ENV === 'production';
 
-if (isProduction || !UsingMockServer) {
-  loadApp();
-}
-else {
-  // load mock server
+
+// load mock server
+if (!isProduction && UsingMockServer) {
   server.start().then(() => {
     loadApp();
   });
 }
+
+// Sentry
+Sentry.init({
+  dsn: "https://e9f254e66aacba9bfb5a6901063e0009@o4508692782907392.ingest.us.sentry.io/4508782708326400",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS_ID);
+
+loadApp();
+
 
 function loadApp() {
   createRoot(document.getElementById('root')!).render(
