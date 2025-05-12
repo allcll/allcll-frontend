ada58b5cc07393938d4ceb0370872e353759f5f6
hyunwoo0081 <hyunwoo0081@gmail.com>
2025-05-12 23:28:36 +0900

    feat: 대시보드 수정

--- packages/client/src/pages/simulation/DashboardDetail.tsx
+++ packages/client/src/pages/simulation/DashboardDetail.tsx
@@ -5,8 +5,9 @@ import { useLiveQuery } from 'dexie-react-hooks';
 import useWishes from '@/hooks/server/useWishes.ts';
 import { getSimulationResult, ResultResponse } from '@/utils/simulation/result.ts';
 import { Wishes } from '@/utils/types.ts';
-import { getStausColor, getWishesColor } from '@/utils/colors.ts';
+import { getStausColor } from '@/utils/colors.ts';
 import { APPLY_STATUS } from '@/utils/simulation/simulation.ts';
+import { findSubjectsById } from '@/utils/subjectPicker.ts';
 
 function DashboardDetail() {
   const { runId } = useParams();
@@ -71,27 +72,25 @@ function SubjectDetailResult({ result }: { result: ExtendedResultResponse }) {
           <th className="py-2 px-2">교수명</th>
           <th className="py-2 px-2">순위</th>
           <th className="py-2 px-2">관심시간</th>
-          <th className="py-2 px-2">관심</th>
+          {/*<th className="py-2 px-2">관심</th>*/}
           <th className="py-2 px-2">성공/실패</th>
         </tr>
       </thead>
       <tbody className="text-gray-700">
         {subject_results.map((row, i) => (
           <tr key={i} className="border-t">
-            <td className="py-2 px-2">{row.subjectInfo?.classCode + '-' + row.subjectInfo?.classCode}</td>
-            <td className="py-2 px-2">{row.subjectInfo?.subjectName}</td>
-            <td className="py-2 px-2">{row.subjectInfo?.professorName}</td>
-            <td className="py-2 px-2">{row.selected_index + '/'}</td>
-            <td className="py-2 px-2">{row.ended_at + ' sec'}</td>
+            <td className="py-2 px-1">{row.subjectInfo?.subjectCode + '-' + row.subjectInfo?.classCode}</td>
+            <td className="py-2 px-1">{row.subjectInfo?.subjectName}</td>
+            <td className="py-2 px-1">{row.subjectInfo?.professorName}</td>
+            <td className="py-2 px-1">{row.selected_index + '/'}</td>
+            <td className="py-2 px-1">{row.ended_at + ' sec'}</td>
+            {/*<td className="py-2 px-1">*/}
+            {/*  <span className={`px-2 py-0.5 rounded-full ${getWishesColor(row.subjectInfo?.totalCount ?? -1)}`}>*/}
+            {/*    {row.subjectInfo?.totalCount}*/}
+            {/*  </span>*/}
+            {/*</td>*/}
             <td className="py-2 px-2">
-              <span
-                className={`px-2 py-0.5 rounded-full text-white ${getWishesColor(row.subjectInfo?.totalCount ?? -1)}`}
-              >
-                {row.subjectInfo?.totalCount}
-              </span>
-            </td>
-            <td className="py-2 px-2">
-              <span className={`px-2 py-0.5 rounded-full text-white text-xs ${getStausColor(row.status)}`}>
+              <span className={`px-2 py-0.5 rounded-full text-xs ${getStausColor(row.status)}`}>
                 {row.status === APPLY_STATUS.SUCCESS ? '성공' : '실패'}
               </span>
             </td>
@@ -139,27 +138,27 @@ function SubjectTimeLine({ result }: { result: ExtendedResultResponse }) {
 }
 
 interface ExtendedResultResponse extends ResultResponse {
-  subject_results: (ResultResponse['subject_results'][number] & { subjectInfo?: Wishes })[];
-  timeline: (ResultResponse['timeline'][number] & { subjectInfo?: Wishes })[];
+  subject_results: (ResultResponse['subject_results'][number] & { subjectInfo?: any })[];
+  timeline: (ResultResponse['timeline'][number] & { subjectInfo?: any })[];
   result?: ExtendedResultResponse;
 }
 
 function joinSubjectInfo(subjects?: Wishes[], result?: ResultResponse | null): ExtendedResultResponse | undefined {
   if (!result || !subjects) return undefined;
 
-  const searchSubject = (subjectId: number) => {
-    return subjects.find(subject => subject.subjectId === subjectId);
-  };
+  // const searchSubject = (subjectId: number) => {
+  //   return subjects.find(subject => subject.subjectId === subjectId);
+  // };
 
   return {
     user_ability: result.user_ability,
     timeline: result.timeline.map(item => ({
       ...item,
-      subjectInfo: searchSubject(item.subject_id),
+      subjectInfo: findSubjectsById(item.subject_id),
     })),
     subject_results: result.subject_results.map(item => ({
       ...item,
-      subjectInfo: searchSubject(item.subject_id),
+      subjectInfo: findSubjectsById(item.subject_id),
     })),
   };
 }
