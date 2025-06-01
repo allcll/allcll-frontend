45001d08422b34e9288544ec93663a6bde220309
songhj10207@naver.com <songhj10207@naver.com>
2025-06-01 12:24:45 +0900

    fix: update game tip

--- packages/client/src/components/simulation/modal/UserWishModal.tsx
+++ packages/client/src/components/simulation/modal/UserWishModal.tsx
@@ -57,9 +57,10 @@ const GameTips = () => (
       게임 <span className="text-blue-500">Tip!</span>
     </h2>
     {[
-      '시작 시 검색 버튼을 빠르게 눌러주세요.',
+      '시작 시 우측 검색 버튼을 빠르게 눌러주세요.',
       '인기 있는 과목을 먼저 잡으세요.',
       '한 번 담은 과목을 또 담지 않도록 주의하세요!',
+      '모든 과목을 담은 즉시 종료됩니다.',
     ].map((tip, idx) => (
       <div key={idx} className="flex items-center">
         <span className="text-blue-500 mr-2">
