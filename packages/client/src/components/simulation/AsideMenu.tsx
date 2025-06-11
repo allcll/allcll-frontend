83322a8955c36c071a74375b691c4e2f64bc80f5
songhj10207@naver.com <songhj10207@naver.com>
2025-06-12 01:27:15 +0900

    chore: disabled unused route

--- packages/client/src/components/simulation/AsideMenu.tsx
+++ packages/client/src/components/simulation/AsideMenu.tsx
@@ -138,15 +138,19 @@ function MediumMenu({ menu }: IMenuComponent) {
 function SmallMenu({ menu }: IMenuComponent) {
   return (
     <li>
-      <NavLink
-        to={menu.path ?? '#'}
-        className={({ isActive }) =>
-          `block px-2 py-2 text-xs cursor-pointer hover:bg-blue-500 hover:text-white ${isActive && menu.path != '#' ? 'bg-blue-500 text-white' : ''}`
-        }
-        end
-      >
-        {menu.name}
-      </NavLink>
+      {menu.path === '#' ? (
+        <div className="block px-2 py-2 text-xs text-gray-400 cursor-not-allowed">{menu.name}</div>
+      ) : (
+        <NavLink
+          to={menu.path ?? '#'}
+          className={({ isActive }) =>
+            `block px-2 py-2 text-xs cursor-pointer hover:bg-blue-500 hover:text-white ${isActive ? 'bg-blue-500 text-white' : ''}`
+          }
+          end
+        >
+          {menu.name}
+        </NavLink>
+      )}
     </li>
   );
 }
