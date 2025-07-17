0f74286641b7fd285e613a0943609aff972955df
songhj10207@naver.com <songhj10207@naver.com>
2025-07-17 15:27:32 +0900

    feat: Checkbox 공통 UI 구현

---
+++ packages/client/src/components/common/Checkbox.tsx
@@ -0,0 +1,30 @@
+import CheckSvg from '@/assets/check-blue.svg?react';
+
+interface ICheckbox {
+  label: string;
+  isChecked: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function Checkbox({ label = 'checkbox', isChecked = false, onChange, ...props }: ICheckbox) {
+  return (
+    <label className="flex flex-row items-center gap-2 text-md cursor-pointer">
+      <div className="relative w-5 h-5">
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={onChange}
+          className={`
+            appearance-none w-5 h-5 rounded-sm border cursor-pointer transition-colors
+            ${isChecked ? 'bg-blue-50 border-blue-500' : 'bg-white border-gray-400'}
+          `}
+          {...props}
+        />
+        {isChecked && <CheckSvg className="absolute inset-0 m-auto w-4 h-4 pointer-events-none text-white" />}
+      </div>
+      <span className={isChecked ? 'text-blue-500' : 'text-gray-700'}>{label}</span>
+    </label>
+  );
+}
+
+export default Checkbox;
