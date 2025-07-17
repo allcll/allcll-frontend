3d8d3dd6439629ac2ec5e27a0b9d2ae5d3bf026d
songhj10207@naver.com <songhj10207@naver.com>
2025-07-17 14:28:27 +0900

    feat: Chip 공통 UI 구현

---
+++ packages/client/src/components/common/Chip.tsx
@@ -0,0 +1,25 @@
+import ArrowIcon from '@/components/svgs/ArrowIcon';
+import CancelIcon from '@/components/svgs/CancelIcon';
+import { getSelectedColor } from '@/utils/colors';
+
+interface IChip {
+  label: string;
+  selected: boolean;
+  type?: 'select' | 'cancle';
+}
+
+function Chip({ label = 'Chip', selected = true, type = 'select', ...props }: IChip) {
+  return (
+    <button
+      type="button"
+      className={`items-center justify-center px-3 py-2 rounded-lg cursor-pointer text-sm gap-4 flex flex-row ${getSelectedColor(selected)} 
+        focus:outline focus:outline-2`}
+      {...props}
+    >
+      <span>{label}</span>
+      {type === 'select' ? <ArrowIcon selected={selected} /> : <CancelIcon selected={selected} />}
+    </button>
+  );
+}
+
+export default Chip;
---
+++ packages/client/src/components/svgs/CancelIcon.tsx
@@ -0,0 +1,12 @@
+import CancelGraySvg from '@/assets/x-gray.svg?react';
+import CancelBlueSvg from '@/assets/x-blue.svg?react';
+
+interface ICancelIcon {
+  className?: string;
+  selected?: boolean;
+}
+const CancelIcon = ({ className, selected = false }: ICancelIcon) => {
+  return selected ? <CancelBlueSvg className={className} /> : <CancelGraySvg className={className} />;
+};
+
+export default CancelIcon;
