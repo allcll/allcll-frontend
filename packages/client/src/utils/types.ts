764ca2405e114a919e9f50fcb9114a76b0637d5d
songhj10207@naver.com <songhj10207@naver.com>
2025-07-23 20:52:37 +0900

    fix: subjects 타입 수정

--- packages/client/src/utils/types.ts
+++ packages/client/src/utils/types.ts
@@ -1,11 +1,3 @@
-export interface Subject {
-  classCode: string; // 분반
-  professorName: string; // 교수명
-  subjectCode: string; // 과목코드
-  subjectId: number; // 과목 ID
-  subjectName: string; // 과목명
-}
-
 export interface PinnedSeats {
   subjectId: number;
   seatCount: number;
@@ -52,6 +44,23 @@ export interface SimulationSubject {
   professorName: string;
   lesn_room: string; // | null;
   tm_num: string;
+}
+
+export interface Subject {
+  subjectId: number; // 과목 ID
+  subjectName: string; // 과목명
+  departmentName: string; // 학과명
+  departmentCode: string; // 학과 코드
+  subjectCode: string; // 과목코드
+  classCode: string; // 분반
+  professorName: string; // 교수명
+  //새로 추가되는 필드
+  language: string; // 수업 언어
+  subjectType: string; // 수업 유형
+  semester_at: number; // 학기
+  lesn_time: string; // 수업 시간
+  lesn_room: string; // 수업실
+  tm_num: string; // 학점
 }
 
 export type SimulationStatusType = 'before' | 'selectedDepartment' | 'start' | 'progress' | 'finish' | 'refresh';
