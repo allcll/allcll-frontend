09795e7ba91ffdc6241ef4529ec39c4d6739c0c6
songhj10207@naver.com <songhj10207@naver.com>
2025-07-24 14:54:07 +0900

    feat: 드롭다운 외 클릭 시 닫히는 훅 구현

--- packages/client/src/components/contentPanel/filter/FilterOption.tsx
+++ packages/client/src/components/contentPanel/filter/FilterOption.tsx
@@ -1,11 +1,15 @@
+import { RefObject } from 'react';
+
 interface IFilterOption {
   children: React.ReactNode;
   className?: string;
+  contentRef: RefObject<HTMLDivElement | null>;
 }
 
-function FilterOption({ children, className = '' }: IFilterOption) {
+function FilterOption({ children, contentRef, className = '' }: IFilterOption) {
   return (
     <div
+      ref={contentRef}
       className={`absolute top-full mt-2 left-0 z-20 bg-white rounded-lg flex flex-col shadow-lg p-4 w-auto gap-1 min-w-max ${className}`}
       role="listbox"
     >
