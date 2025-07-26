9579acb240f5ceb9ddbb1ebc454cffbafaa2fa58
songhj10207@naver.com <songhj10207@naver.com>
2025-07-27 03:26:03 +0900

    fix: 과목 Info 모달 정보 추가

--- packages/client/src/components/contentPanel/ScheduleInfoModal.tsx
+++ packages/client/src/components/contentPanel/ScheduleInfoModal.tsx
@@ -3,10 +3,12 @@ import { useBottomSheetStore } from '@/store/useBottomSheetStore';
 import XDarkGraySvg from '@/assets/x-darkgray.svg?react';
 import ClockGraySvg from '@/assets/clock-gray.svg?react';
 import HouseSvg from '@/assets/house.svg?react';
+import useSubject from '@/hooks/server/useSubject';
 
 function ScheduleInfoModal() {
   const { schedule, deleteSchedule } = useScheduleModal();
   const { closeBottomSheet } = useBottomSheetStore();
+  const { data: subjects } = useSubject();
 
   const handleDeleteOfficialSchedule = (e: React.MouseEvent<HTMLButtonElement>) => {
     const confirmed = confirm('해당 과목을 삭제하시겠습니까?');
@@ -14,6 +16,8 @@ function ScheduleInfoModal() {
 
     deleteSchedule(e);
   };
+
+  const findSubjectById = subjects?.find(subject => subject.subjectId === schedule.subjectId);
 
   return (
     <div className="fixed inset-0 z-50  hidden md:flex items-center justify-center  bg-black/10">
@@ -33,20 +37,26 @@ function ScheduleInfoModal() {
           </button>
         </div>
 
-        <p className="text-sm text-gray-500">{schedule.professorName}</p>
-        <div className="flex items-center gap-1">
-          <ClockGraySvg className="w-4 h-4 " />
-          {schedule.timeSlots.map(slot => {
-            return (
-              <span className="text-gray-500 text-sm">
-                {slot.dayOfWeeks} {slot.startTime} ~ {slot.endTime}
-              </span>
-            );
-          })}
-        </div>
-        <div className="flex items-center gap-1">
-          <HouseSvg className="w-4 h-4 " />
-          <span className="text-gray-500 text-sm">{schedule.location}</span>
+        <div className="flex flex-col gap-1 px-2 py-3 text-gray-500 text-sm">
+          <p className="text-sm text-gray-500">{findSubjectById?.professorName ?? '교수 정보 없음'}</p>
+          <div className="flex items-center gap-1">
+            <ClockGraySvg className="w-4 h-4 text-gray-400" />
+            <span className="text-gray-500 text-sm">{findSubjectById?.lesnTime}</span>;
+          </div>
+          <div className="flex items-center gap-1">
+            <HouseSvg className="w-4 h-4 text-gray-400" />
+            <span className="text-gray-500 text-sm">{findSubjectById?.lesnRoom ?? '장소 정보 없음'}</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <span className="text-gray-500 text-sm">{findSubjectById?.manageDeptNm}</span>
+            <span className="text-gray-500 text-sm"> {findSubjectById?.studentYear + '학년'}</span>
+
+            <span className="text-blue-500 text-sm">{findSubjectById?.tmNum[0] + '학점'}</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <span className="text-gray-500 text-sm">{findSubjectById?.language}</span>
+            <span className="text-gray-500 text-sm">{findSubjectById?.subjectType}</span>
+          </div>
         </div>
         <div className="px-4 py-4">
           <button
--- packages/client/src/components/contentPanel/bottomSheet/ScheduleDetailBottomSheet.tsx
+++ packages/client/src/components/contentPanel/bottomSheet/ScheduleDetailBottomSheet.tsx
@@ -2,12 +2,14 @@ import BottomSheet from './BottomSheet';
 import BottomSheetHeader from './BottomSheetHeader';
 import ClockGraySvg from '@/assets/clock-gray.svg?react';
 import HouseSvg from '@/assets/house.svg?react';
+import useSubject from '@/hooks/server/useSubject';
 import useScheduleModal from '@/hooks/useScheduleModal';
 import { useBottomSheetStore } from '@/store/useBottomSheetStore';
 
 function ScheduleInfoBottomSheet() {
   const { schedule, deleteSchedule } = useScheduleModal();
   const { closeBottomSheet } = useBottomSheetStore();
+  const { data: subjects } = useSubject();
 
   const handleDeleteOfficialSchedule = (e: React.MouseEvent<HTMLButtonElement>) => {
     const confirmed = confirm('해당 과목을 삭제하시겠습니까?');
@@ -16,28 +18,34 @@ function ScheduleInfoBottomSheet() {
     deleteSchedule(e);
   };
 
+  const findSubjectById = subjects?.find(subject => subject.subjectId === schedule.subjectId);
+
   return (
     <BottomSheet>
       <BottomSheetHeader headerType="close" onClose={() => closeBottomSheet()} />
       <div className="w-full flex items-center gap-2 border-b border-gray-200 p-2 h-12">
-        <h3 className="font-semibold text-md">{schedule.subjectName}</h3>
+        <h3 className="font-semibold text-md">{findSubjectById?.subjectName}</h3>
       </div>
 
       <div className="flex flex-col gap-1 px-2 py-3 text-gray-500 text-sm">
-        <p className="text-sm text-gray-500">{schedule.professorName}</p>
+        <p className="text-sm text-gray-500">{findSubjectById?.professorName ?? '교수 정보 없음'}</p>
         <div className="flex items-center gap-1">
           <ClockGraySvg className="w-4 h-4 text-gray-400" />
-          {schedule.timeSlots.map(slot => {
-            return (
-              <span className="text-gray-500 text-sm">
-                {slot.dayOfWeeks} {slot.startTime} ~ {slot.endTime}
-              </span>
-            );
-          })}
+          <span className="text-gray-500 text-sm">{findSubjectById?.lesnTime}</span>;
         </div>
         <div className="flex items-center gap-1">
           <HouseSvg className="w-4 h-4 text-gray-400" />
-          <span className="text-gray-500 text-sm">{schedule.location}</span>
+          <span className="text-gray-500 text-sm">{findSubjectById?.lesnRoom ?? '장소 정보 없음'}</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <span className="text-gray-500 text-sm">{findSubjectById?.manageDeptNm}</span>
+          <span className="text-gray-500 text-sm"> {findSubjectById?.studentYear + '학년'}</span>
+
+          <span className="text-blue-500 text-sm">{findSubjectById?.tmNum[0] + '학점'}</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <span className="text-gray-500 text-sm">{findSubjectById?.language}</span>
+          <span className="text-gray-500 text-sm">{findSubjectById?.subjectType}</span>
         </div>
       </div>
       <div className="px-4 py-4">
