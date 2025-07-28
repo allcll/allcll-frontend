af3683c290a89788b27c69c7070a096e471b7fc8
songhj10207@naver.com <songhj10207@naver.com>
2025-07-28 16:24:01 +0900

    fix: 검색 결과 없음 UI 수정

--- packages/client/src/components/contentPanel/errors/ZeroListError.tsx
+++ packages/client/src/components/contentPanel/errors/ZeroListError.tsx
@@ -7,7 +7,7 @@ function ZeroListError() {
   const initSchedule = new ScheduleAdapter().toUiData();
 
   return (
-    <div className="flex flex-col h-full gap-2 pt-5 w-full items-center justify-center">
+    <div className="flex flex-col gap-2 pt-5 w-full items-center justify-center">
       <SearchSvg className="w-7 h-7" />
       <p className="text-md text-gray-500">검색 결과가 없습니다.</p>
       <span className="text-xs text-gray-400">찾으시는 과목이 없으신가요? 과목을 직접 등록해주세요.</span>
