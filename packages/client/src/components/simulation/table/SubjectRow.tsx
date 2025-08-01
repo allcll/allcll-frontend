5654bdfca38b5ba87d38c88b5222e2015553f909
songhj10207@naver.com <songhj10207@naver.com>
2025-08-01 16:06:59 +0900

    fix: 시뮬레이션 결과 모달 수정

--- packages/client/src/components/simulation/table/SubjectRow.tsx
+++ packages/client/src/components/simulation/table/SubjectRow.tsx
@@ -11,10 +11,10 @@ const SubjectRow = ({ index, subject, isRegisteredTable, onClickSubject }: Subje
   return (
     <tr className="hover:bg-gray-50 text-nowrap">
       <td className="border bg-blue-100 px-2 py-1 border-gray-200">{index + 1}</td>
-      <td className="border px-2 py-1 border-gray-200">
+      <td className="border py-1 border-gray-200">
         <button
-          className="bg-blue-500 cursor-pointer text-white text-xs px-2 py-0.5 rounded-xs"
-          disabled={isRegisteredTable}
+          className="bg-blue-500 cursor-pointer text-white text-xs px-3 py-0.5 rounded-xs"
+          disabled={!!isRegisteredTable}
           onClick={() => onClickSubject(subject.subjectId)}
         >
           {isRegisteredTable ? '삭제' : '신청'}
@@ -22,13 +22,13 @@ const SubjectRow = ({ index, subject, isRegisteredTable, onClickSubject }: Subje
       </td>
       <td className="border px-2 py-1 border-gray-200">{subject.subjectCode}</td>
       <td className="border px-2 py-1 border-gray-200">{subject.classCode}</td>
-      <td className="border px-2 py-1 border-gray-200">{subject.departmentName}</td>
-      <td className="border px-2 py-1 border-gray-200">{subject.subjectName}</td>
+      <td className="border px-2 py-1 border-gray-200 text-left">{subject.departmentName}</td>
+      <td className="border px-2 py-1 border-gray-200 text-left">{subject.subjectName}</td>
       <td className="border px-2 py-1 border-gray-200">
         <button className="bg-gray-700 text-xs text-white rounded-xs px-2 py-0.5">수업계획서</button>
       </td>
-      <td className="border px-2 py-1 border-gray-200">{subject.language || '-'}</td>
-      <td className="border px-2 py-1 border-gray-200">
+      <td className="border py-1 border-gray-200">{subject.language || '-'}</td>
+      <td className="border  py-1 border-gray-200">
         {subject.tm_num
           ? subject.tm_num
               .split('/')
@@ -39,7 +39,7 @@ const SubjectRow = ({ index, subject, isRegisteredTable, onClickSubject }: Subje
       <td className="border px-2 py-1 border-gray-200">{subject.subjectType}</td>
       <td className="border px-2 py-1 border-gray-200">{subject.semester_at === -1 ? '' : subject.semester_at}</td>
       <td className="border px-2 py-1 border-gray-200">{subject.lesn_time || '-'}</td>
-      <td className="border px-2 py-1 border-gray-200">
+      <td className="border py-1 border-gray-200">
         <button className="bg-gray-700 text-white text-xs px-2 py-0.5 rounded-xs">수강인원</button>
       </td>
     </tr>
