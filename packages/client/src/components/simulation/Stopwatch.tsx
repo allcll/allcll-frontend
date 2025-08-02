82e2a478e2774c54bb51cb36fe3cbde277afbaf7
songhj10207@naver.com <songhj10207@naver.com>
2025-08-02 18:34:25 +0900

    feat: 5분 지나면 시뮬레이션 종료 설정

--- packages/client/src/components/simulation/Stopwatch.tsx
+++ packages/client/src/components/simulation/Stopwatch.tsx
@@ -1,16 +1,29 @@
+import { useSimulationModalStore } from '@/store/simulation/useSimulationModal';
 import useSimulationProcessStore from '@/store/simulation/useSimulationProcess';
-import { getOngoingSimulation } from '@/utils/simulation/simulation';
+import { forceStopSimulation, getOngoingSimulation } from '@/utils/simulation/simulation';
 import { useState, useEffect, useRef } from 'react';
 
 function Stopwatch() {
   const [currentTime, setCurrentTime] = useState('00:00');
-  const { currentSimulation } = useSimulationProcessStore();
+  const { openModal } = useSimulationModalStore();
+  const { currentSimulation, setCurrentSimulation } = useSimulationProcessStore();
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   const getTime = (totalSeconds: number) => {
     const minutes = Math.floor((totalSeconds % 3600) / 60);
     const seconds = totalSeconds % 60;
     setCurrentTime(`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`);
+  };
+
+  const forceSimulation = async () => {
+    try {
+      await forceStopSimulation();
+      setCurrentSimulation({ simulationStatus: 'finish' });
+      openModal('result');
+    } catch (error) {
+      console.error(error);
+      alert('시뮬레이션 강제 종료에 실패했습니다.');
+    }
   };
 
   useEffect(() => {
@@ -31,6 +44,13 @@ function Stopwatch() {
         intervalRef.current = setInterval(() => {
           const now = Date.now();
           const seconds = Math.floor((now - start) / 1000);
+
+          if (seconds >= 5 * 60) {
+            clearInterval(intervalRef.current!);
+            alert('5분 경과로 시뮬레이션이 강제 종료되었습니다.');
+            forceSimulation();
+            return;
+          }
 
           getTime(seconds);
 
