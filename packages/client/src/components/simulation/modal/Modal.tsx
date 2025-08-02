3e7db932ed1c466c48a298a9b83184f526215b1b
songhj10207@naver.com <songhj10207@naver.com>
2025-08-03 01:48:58 +0900

    fix: 모달 뒷 배경 스크롤 막기

--- packages/client/src/components/simulation/modal/Modal.tsx
+++ packages/client/src/components/simulation/modal/Modal.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 interface IModalProps {
   children: React.ReactNode;
 }
 
 function Modal({ children }: IModalProps) {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, []);
+
   return (
     <>
       <div className="fixed top-0 left-0 w-full h-full bg-gray-300 opacity-30 z-40"></div>
