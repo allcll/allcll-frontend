c251aa8628d188508aaf1b83cff309c963596498
songhj10207@naver.com <songhj10207@naver.com>
2025-08-10 00:23:56 +0900

    fix: 어드민 개발서버 프록시 설정

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -1,27 +1,33 @@
-import { defineConfig } from 'vite';
+import { ConfigEnv, defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss(), svgr()],
-  server: {
-    fs: {
-      allow: ['..'],
-    },
+export default ({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const TargetHost = env.VITE_TARGET_HOST ?? 'localhost:8080';
+  const TargetServer = `https://${TargetHost}`;
+
+  return defineConfig({
+    plugins: [react(), tsconfigPaths(), tailwindcss(), svgr()],
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: TargetServer,
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      '/admin': {
+        target: TargetServer,
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, '')
       },
       '/sse': {
-        target: 'http://localhost:3000',
+        target: TargetServer,
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/sse/, '')
       },
     },
-  },
-});
+  });
+};
