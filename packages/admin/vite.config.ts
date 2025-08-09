5abb322c74540c5571abfe45b0d5b981fd72857f
songhj10207@naver.com <songhj10207@naver.com>
2025-08-10 03:34:17 +0900

    feat: queryClient 및 목서버 설정

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -3,30 +3,38 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import dotenv from 'dotenv';
 
 export default ({ mode }: ConfigEnv) => {
   const env = loadEnv(mode, process.cwd());
 
-  const TargetHost = env.VITE_TARGET_HOST ?? 'localhost:8080';
+  const TargetHost = env.VITE_TARGET_HOST;
   const TargetServer = `https://${TargetHost}`;
+  dotenv.config();
 
   return defineConfig({
-    plugins: [react(), tsconfigPaths(), tailwindcss(), svgr()],
-    proxy: {
-      '/api': {
-        target: TargetServer,
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '')
+    plugins: [react(), tsconfigPaths(), tailwindcss(), svgr(), basicSsl()],
+    server: {
+      open: true,
+      https: {
+        key: '.cert/localhost-key.pem',
+        cert: '.cert/localhost.pem',
       },
-      '/admin': {
-        target: TargetServer,
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '')
-      },
-      '/sse': {
-        target: TargetServer,
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/sse/, '')
+      proxy: {
+        '/api': {
+          target: TargetServer,
+          changeOrigin: true,
+        },
+        '/admin': {
+          target: TargetServer,
+          changeOrigin: true,
+          // rewrite: path => path.replace(/^\/api/, ''), // '/api' 경로 제거
+        },
+        '/sse': {
+          target: TargetServer,
+          changeOrigin: true,
+        },
       },
     },
   });
