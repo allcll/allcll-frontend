3cd4448a39a6ed597b3988a7228017c691683229
hyunwoo0081 <hyunwoo0081@gmail.com>
2025-08-20 20:08:07 +0900

    feat: 과목 필터링 로직 추가

--- packages/client/src/utils/filtering/filterSubjects.ts
+++ packages/client/src/utils/filtering/filterSubjects.ts
@@ -1,5 +1,6 @@
 import { disassemble } from 'es-hangul';
 import { Day, Grade, Subject, Wishes } from '../types';
+import { IDayTimeItem } from '@/components/contentPanel/filter/DayTimeFilter.tsx';
 
 export function filterDays(subject: Wishes | Subject, selectedDays: (Day | '전체')[]) {
   if (!subject.lesnTime) {
@@ -53,4 +54,67 @@ export function filterSearchKeywords(subject: Wishes | Subject, cleanedKeyword: 
     disassembledSubjectName.includes(cleanedKeyword) ||
     filteredBySubjectAndClassCode
   );
+}
+
+export function filterClassroom(subject: Wishes | Subject, selectedClassrooms: string[]) {
+  if (!subject.lesnRoom || selectedClassrooms.length === 0) {
+    return false;
+  }
+
+  const abbreviatedRooms = selectedClassrooms.map(r => (r === '대양AI센터' ? '센' : r.slice(0, 1)));
+
+  return abbreviatedRooms.some(room => {
+    const roomRegex = new RegExp(`^${room}[\d\w]`, 'i');
+    return roomRegex.test(subject.lesnRoom);
+  });
+}
+
+export function filterRemark(subject: Wishes | Subject, selectedNotes: string[]) {
+  if (!selectedNotes || selectedNotes.length === 0) {
+    return false;
+  }
+
+  return selectedNotes.some(note => {
+    switch (note) {
+      case '외국인대상':
+        return subject.remark?.includes('외국인') ?? false;
+      case 'SHP대상':
+        return ['SHP', 'Honor'].some(k => subject.remark?.includes(k) ?? false);
+      case '기타':
+        return !subject.remark || !['외국인', 'SHP', 'Honor'].some(k => subject.remark?.includes(k) ?? false);
+    }
+  });
+}
+
+export function filterSchedule(subject: Wishes | Subject, selectedTime: IDayTimeItem[]) {
+  if (!selectedTime || selectedTime.length === 0 || !subject.lesnTime) {
+    return true;
+  }
+
+  return selectedTime.some(item => {
+    if (item.day === '') return true;
+
+    if (item.type === 'all') return subject.lesnTime.includes(item.day);
+
+    const regex = new RegExp(`${item.day}(\\d{2}:\\d{2})`);
+    const lessonTime = subject.lesnTime.match(regex);
+
+    if (!lessonTime) return false;
+
+    if (item.type === 'before' && item.start) {
+      return lessonTime.some(time => item.start.localeCompare(time) <= 0);
+    }
+
+    if (item.type === 'after' && item.end) {
+      return lessonTime.some(time => item.end.localeCompare(time) >= 0);
+    }
+
+    if (item.type === 'between' && item.start && item.end) {
+      return lessonTime.some(time => item.start.localeCompare(time) <= 0 && item.end.localeCompare(time) >= 0);
+    }
+  });
+}
+
+export function filterCategory(subject: Wishes | Subject, selectedCategories: string[]) {
+  return !selectedCategories.length || selectedCategories.includes(subject.curiTypeCdNm);
 }
