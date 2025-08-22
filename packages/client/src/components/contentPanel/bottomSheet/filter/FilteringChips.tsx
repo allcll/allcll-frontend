40e7693ef7943685af8b5a38a641098eaa8d44f4
songhj10207@naver.com <songhj10207@naver.com>
2025-08-22 17:10:01 +0900

    fix: 모바일 필터링 칩 추가

---
+++ packages/client/src/components/contentPanel/bottomSheet/filter/FilteringChips.tsx
@@ -0,0 +1,47 @@
+import Chip from '@common/components/chip/Chip';
+import { OptionType } from '@common/components/filtering/SingleCheckbox';
+
+interface IFilterChips<VALUE extends string | number> {
+  label: string;
+  field: string;
+  selectedValue: VALUE[];
+  setFilterSchedule: (field: string, value: VALUE[]) => void;
+  options: OptionType<VALUE>[];
+  className?: string;
+}
+
+function FilterChips<VALUE extends string | number>({
+  label,
+  field,
+  selectedValue,
+  setFilterSchedule,
+
+  options,
+}: Readonly<IFilterChips<VALUE>>) {
+  const handleChangeChip = (optionValue: VALUE) => {
+    const selected = selectedValue.includes(optionValue);
+    const newValues = selected
+      ? selectedValue.filter(selected => selected !== optionValue)
+      : [...selectedValue, optionValue];
+
+    setFilterSchedule(field, newValues);
+  };
+
+  return (
+    <div className="w-full flex gap-2 flex-col justify-center">
+      <label className="text-xs text-gray-500">{label}</label>
+      <div className="flex gap-2 flex-wrap">
+        {options.map(option => (
+          <Chip
+            key={option.value}
+            label={option.label}
+            selected={selectedValue.includes(option.value)}
+            onClick={() => handleChangeChip(option.value)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default FilterChips;
