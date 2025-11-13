d4feb6ac6a210de4aef92df7fa4597237484e787
songhj10207@naver.com <songhj10207@naver.com>
2025-11-14 02:16:33 +0900

    feat: 배너 컴포넌트 추가

---
+++ packages/allcll-ui/src/components/banner/Banner.tsx
@@ -0,0 +1,35 @@
+import ImportantSvg from '@/assets/important.svg?react';
+import CloseSvg from '@/assets/x-gray.svg?react';
+
+interface IBanner {
+  variant?: 'warning' | 'info';
+  children: React.ReactNode;
+  deleteBanner: () => void;
+}
+
+function Banner({ variant, children, deleteBanner }: IBanner) {
+  const bannerColorClass = getBannerColor(variant);
+
+  return (
+    <div className={`flex items-center justify-between px-4 py-2 gap-2 ${bannerColorClass}`}>
+      <ImportantSvg className="w-4 h-4" />
+      <div className="flex-auto text-xs">{children}</div>
+      <button className="p-1 rounded-full hover:bg-blue-200" onClick={deleteBanner} aria-label="알림 닫기">
+        <CloseSvg className="" />
+      </button>
+    </div>
+  );
+}
+
+export default Banner;
+
+function getBannerColor(variant: IBanner['variant']) {
+  switch (variant) {
+    case 'warning':
+      return 'bg-secondary-100';
+    case 'info':
+      return 'bg-blue-100';
+    default:
+      return 'bg-gray-100';
+  }
+}
