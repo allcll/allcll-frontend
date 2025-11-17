95c6bd7d6de68ec18a5be20f4a43fb19043207c5
hyunwoo0081 <hyunwoo0081@gmail.com>
2025-11-17 16:28:39 +0900

    feat: index 생성된 컴포넌트 추가

---
+++ packages/sejong-ui/index.ts
@@ -0,0 +1,10 @@
+import Button from './src/Button';
+import SectionHeader from './src/SectionHeader';
+import Tab from './src/Tab';
+import type { ITab } from './src/Tab';
+import AsideMenu from './src/asideMenu';
+import type { IMenu } from './src/asideMenu';
+import DataTable from './src/DataTable/DataTable';
+
+export { Button, SectionHeader, Tab, AsideMenu, DataTable };
+export type { IMenu, ITab };
