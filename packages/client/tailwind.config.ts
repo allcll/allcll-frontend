2b79cb268c9a27323ef802c46f5af61fc9dc3fa2
songhj10207@naver.com <songhj10207@naver.com>
2025-12-13 17:25:38 +0900

    fix: allcll-ui 패키지 의존성 추가

--- packages/client/tailwind.config.ts
+++ packages/client/tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
   content: [
     './src/**/*.{html,js,jsx,ts,tsx}',
     '../common/src/**/*.{ts,tsx,js,jsx}',
+    // 현재는 모노레포 구조라 로컬 디자인 시스템 소스를 직접 참조함.
     '../allcll-ui/**/*.{ts,tsx,js,jsx}',
   ],
   theme: {
