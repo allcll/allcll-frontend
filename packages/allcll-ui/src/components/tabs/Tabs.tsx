45374487fa802e99de05fd870db78e80bafa4896
songhj10207@naver.com <songhj10207@naver.com>
2025-12-15 14:34:04 +0900

    fix: Tab 스타일 수정

--- packages/allcll-ui/src/components/tabs/Tabs.tsx
+++ packages/allcll-ui/src/components/tabs/Tabs.tsx
@@ -1,6 +1,6 @@
 function Tabs({ children }: { children: React.ReactNode }) {
   return (
-    <ul role="tablist" className="flex space-x-4 text-sm sm:text-base">
+    <ul role="tablist" className="flex space-x-5 py-5 px-2 text-sm sm:text-base border-b border-gray-200">
       {children}
     </ul>
   );
