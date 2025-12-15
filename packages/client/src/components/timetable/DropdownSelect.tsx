1276b2e1b796425373aab9edaef781f6637f8e08
songhj10207@naver.com <songhj10207@naver.com>
2025-12-15 15:43:23 +0900

    fix: 시간표 선택 컴포넌트 수정

--- packages/client/src/components/timetable/DropdownSelect.tsx
+++ packages/client/src/components/timetable/DropdownSelect.tsx
@@ -1,35 +1,31 @@
-import { TimetableType } from '@/hooks/server/useTimetableSchedules.ts';
+import { TimetableType, useDeleteTimetable } from '@/hooks/server/useTimetableSchedules';
 import { useScheduleState } from '@/store/useScheduleState';
 import { Button, Checkbox, Flex, Popover, SupportingText } from '@allcll/allcll-ui';
 
 interface DropdownSelectProps {
   timetables: TimetableType[];
   onSelect: (optionId: number) => void;
-  onEdit?: (value: string, optionId: number) => void;
-  onDelete?: (optionId: number) => void;
+  setIsOpenModal: React.Dispatch<React.SetStateAction<'edit' | 'create' | null>>;
   openCreateModal?: () => void;
 }
 
 // Fixme : 기존에 있는 Chip 형태의 Selectbox 와 통합하기
-const DropdownSelect = ({ timetables, onSelect, onEdit, onDelete, openCreateModal }: DropdownSelectProps) => {
+const DropdownSelect = ({ timetables, onSelect, setIsOpenModal, openCreateModal }: DropdownSelectProps) => {
   const currentTimetable = useScheduleState(state => state.currentTimetable);
   const setCurrentTimetable = useScheduleState(state => state.pickTimetable);
+  const { mutate: deleteTimetable } = useDeleteTimetable();
 
   const handleOptionClick = (option: TimetableType) => {
     setCurrentTimetable(option);
     onSelect(option.timeTableId);
   };
 
-  const handleEditClick = (optionId: number) => {
-    if (onEdit) {
-      onEdit('', optionId);
-    }
+  const handleEditClick = () => {
+    setIsOpenModal('edit');
   };
 
   const handleDeleteClick = (optionId: number) => {
-    if (onDelete) {
-      onDelete(optionId);
-    }
+    deleteTimetable(optionId);
   };
 
   return (
@@ -48,7 +44,7 @@ const DropdownSelect = ({ timetables, onSelect, onEdit, onDelete, openCreateModa
               />
               {currentTimetable.timeTableId === option.timeTableId && (
                 <Flex gap="gap-4">
-                  <Button variant="text" size="small" onClick={() => handleEditClick(option.timeTableId)}>
+                  <Button variant="text" size="small" onClick={handleEditClick}>
                     수정
                   </Button>
                   <Button
@@ -65,7 +61,7 @@ const DropdownSelect = ({ timetables, onSelect, onEdit, onDelete, openCreateModa
           ))}
 
           <Button variant="text" size="small" textColor="gray" onClick={openCreateModal}>
-            + 시간표 추가하기
+            + 시간표 만들기
           </Button>
         </Flex>
       </Popover.Content>
