94c6b4a6f0fb1a1bd8289dbdc443be5103616019
songhj10207@naver.com <songhj10207@naver.com>
2025-12-18 02:31:41 +0900

    fix: 시간표 폼 바텀 시트에 버튼 액션 추가

--- packages/client/src/widgets/bottomSheet/FormBottomSheet.tsx
+++ packages/client/src/widgets/bottomSheet/FormBottomSheet.tsx
@@ -4,17 +4,19 @@ import BottomSheetHeader from '../../shared/ui/bottomsheet/BottomSheetHeader.tsx
 import ScheduleFormContent from '../../components/contentPanel/ScheduleFormContent.tsx';
 import useScheduleModal, { useScheduleModalData } from '@/hooks/useScheduleModal.ts';
 import { ScheduleMutateType } from '@/store/useScheduleState.ts';
-import { Flex } from '../../../../allcll-ui';
+import { Button, Flex } from '@allcll/allcll-ui';
+import useMobile from '@/shared/lib/useMobile.ts';
 
 function FormBottomSheet() {
   const { modalActionType } = useScheduleModalData();
-  const { cancelSchedule } = useScheduleModal();
+  const { cancelSchedule, deleteSchedule } = useScheduleModal();
 
   const handleCancelSchedule = (e: React.MouseEvent<HTMLButtonElement>) => {
     cancelSchedule(e);
   };
 
   const title = modalActionType === ScheduleMutateType.CREATE ? '생성' : '수정';
+  const isMobile = useMobile();
 
   return (
     <BottomSheet>
@@ -22,6 +24,16 @@ function FormBottomSheet() {
       <Flex direction="flex-col" className="py-5 px-2 overflow-y-auto max-h-[80vh]">
         <ScheduleFormContent />
       </Flex>
+
+      {(modalActionType === ScheduleMutateType.EDIT || modalActionType === ScheduleMutateType.VIEW) && (
+        <Button variant="secondary" size={isMobile ? 'small' : 'medium'} onClick={deleteSchedule}>
+          삭제
+        </Button>
+      )}
+
+      <Button type="submit" variant="primary" size={isMobile ? 'small' : 'medium'}>
+        저장
+      </Button>
     </BottomSheet>
   );
 }
