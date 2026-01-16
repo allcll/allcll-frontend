09cc9a66b49ddc482186a356a619a785cdf3942e
songhj10207@naver.com <songhj10207@naver.com>
2026-01-16 12:44:58 +0900

    hotfix: 컴포넌트 렌더링 전 라우팅 처리

--- packages/client/src/features/timetable/ui/TimetableSemesterTabs.tsx
+++ packages/client/src/features/timetable/ui/TimetableSemesterTabs.tsx
@@ -1,16 +1,11 @@
 import { Flex } from '@allcll/allcll-ui';
-import { Link, Navigate, useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
-import { SEMESTERS, SERVICE_SEMESTER_DUMMY } from '@/entities/semester/api/semester.ts';
+import { SEMESTERS } from '@/entities/semester/api/semester.ts';
 
 function TimetableSemesterTabs() {
   const [searchParams] = useSearchParams();
   const currentSemester = searchParams.get('semester');
-
-  // Todo: 훅으로 분리
-  if (!currentSemester) {
-    return <Navigate to={`/timetable?semester=${SERVICE_SEMESTER_DUMMY.semester}`} replace />;
-  }
 
   const activeClassName = 'border-b-2 border-primary text-primary pb-2';
 
