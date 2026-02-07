2a9ae9007ed560bafd89b42e3abe2c82c3aae973
hyunwoo0081 <hyunwoo0081@gmail.com>
2026-02-07 19:54:58 +0900

    feat: 졸업요건 title 추가

--- packages/client/src/pages/joluphaja/Dashboard.tsx
+++ packages/client/src/pages/joluphaja/Dashboard.tsx
@@ -1,7 +1,16 @@
 import GraduationDashboardPage from '@/pages/graduation/GraduationDashboardPage.tsx';
+import { Helmet } from 'react-helmet';
 
 function Dashboard() {
-  return <GraduationDashboardPage />;
+  return (
+    <>
+      <Helmet>
+        <title>ALLCLL | 졸업요건검사 결과</title>
+        <meta name="description" content="세종대학교 졸업요건검사를 도와드립니다." />
+      </Helmet>
+      <GraduationDashboardPage />
+    </>
+  );
 }
 
 export default Dashboard;
--- packages/client/src/pages/joluphaja/Graduation.tsx
+++ packages/client/src/pages/joluphaja/Graduation.tsx
@@ -1,7 +1,16 @@
 import Steps from '@/features/jolup/ui/Steps';
+import { Helmet } from 'react-helmet';
 
 function Graduation() {
-  return <Steps />;
+  return (
+    <>
+      <Helmet>
+        <title>ALLCLL | 졸업요건검사</title>
+        <meta name="description" content="세종대학교 졸업요건검사를 도와드립니다." />
+      </Helmet>
+      <Steps />
+    </>
+  );
 }
 
 export default Graduation;
