0652fca6a26f3ffe986e444001e1383a190ce89a
songhj10207@naver.com <songhj10207@naver.com>
2026-02-11 00:38:02 +0900

    Revert "fix: 실시간 페이지 알맞은 메시지 출력"
    This reverts commit 63b9f60650e81f27273e3b26dda0477e67b5e9e6.

--- packages/client/src/widgets/live/_errors/ui/PreSeatWillAvailable.tsx
+++ packages/client/src/widgets/live/_errors/ui/PreSeatWillAvailable.tsx
@@ -1,27 +1,12 @@
 import NoneLayout from '@/shared/ui/NoneLayout';
 import AlarmSVG from '@/assets/alarm.svg?react';
 import { Flex } from '@allcll/allcll-ui';
-import useServiceSemester from '@/entities/semester/model/useServiceSemester';
 
 function PreSeatWillAvailable() {
-  const { data } = useServiceSemester('live');
-
-  const endedService = data && 'service' in data && data.service && !data.service.withinPeriod ? data.service : null;
-
-  if (!endedService) {
-    return (
-      <NoneLayout
-        title="수강 신청이 종료되었습니다."
-        description="수강 신청이 시작되면 실시간 좌석 현황을 확인할 수 있어요."
-        icon={<AlarmSVG className="w-7 h-7 text-gray-400" />}
-      />
-    );
-  }
-
   return (
     <NoneLayout
       title="수강 정정 기간이 종료되었습니다"
-      description={`수강 신청 고생 많으셨습니다. 다음 학기에 만나요.`}
+      description="수강 신청 고생 많으셨습니다. 다음 학기에 만나요."
       icon={<AlarmSVG className="w-7 h-7 text-gray-400" />}
     >
       <Flex justify="justify-center">
