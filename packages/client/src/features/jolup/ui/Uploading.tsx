a0a8a7dcade0c2e6a9ec5f8e9a019339af008135
INSANE-P <chanbin0626@gmail.com>
2026-02-14 23:13:00 +0900

    fix: 업로딩 화면 메세지 변경시 프로그레스 바 레이아웃 정렬 오류 수정

--- packages/client/src/features/jolup/ui/Uploading.tsx
+++ packages/client/src/features/jolup/ui/Uploading.tsx
@@ -19,10 +19,10 @@ function Uploading({ nextStep, prevStep, file }: UploadingProps) {
           </Heading>
         </div>
 
-        <div className="space-y-3">
-          <Flex direction="flex-col" justify="justify-between" align="items-end">
+        <div className="space-y-3 w-full">
+          <div className="text-center min-h-[20px]">
             <span className="text-sm font-medium text-gray-500">{message}</span>
-          </Flex>
+          </div>
 
           <Flex justify="justify-center" gap="gap-4">
             <span className="text-xl font-bold text-primary">{progress}%</span>
