d3c95588e4bd193123652e8a46dbc3b606d84aa9
songhj10207@naver.com <songhj10207@naver.com>
2026-02-16 18:33:59 +0900

    fix: [QA-114] 26학번 학과 선택 예외 처리

--- packages/client/src/features/graduation/ui/setup/BasicInfoForm.tsx
+++ packages/client/src/features/graduation/ui/setup/BasicInfoForm.tsx
@@ -126,6 +126,10 @@ const BasicInfoForm = ({ nextStep, prevStep, isDepartmentNotFound }: BasicInfoFo
           {/* 주전공 학과 검색 (DEPARTMENT_NOT_FOUND 시) */}
           {isDepartmentNotFound && (
             <Flex direction="flex-col" gap="gap-2">
+              <SupportingText>
+                <span className="text-primary">26학번의 계열 단위</span>는 특정한 졸업요건이 존재하지 않아, 학과를
+                선택해주셔야합니다.
+              </SupportingText>
               <Label required>주전공 학과</Label>
               <div className="relative">
                 <SearchBox
