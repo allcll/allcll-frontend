aea3077980998ae65f98563d1627ce4073d8a726
hyunwoo0081 <hyunwoo0081@gmail.com>
2026-03-01 00:36:42 +0900

    fix: change zustand persist

--- packages/client/src/features/feedback/model/useFeedbackStore.ts
+++ packages/client/src/features/feedback/model/useFeedbackStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 type FeedbackStore = {
   dontShowAgain: boolean;
@@ -7,24 +8,18 @@ type FeedbackStore = {
 
 const STORAGE_KEY = 'checll_feedback_dont_show';
 
-export const useFeedbackStore = create<FeedbackStore>(set => ({
-  dontShowAgain: (() => {
-    try {
-      const raw = localStorage.getItem(STORAGE_KEY);
-      return raw === 'true';
-    } catch (e) {
-      return false;
+export const useFeedbackStore = create<FeedbackStore>()(
+  persist(
+    set => ({
+      dontShowAgain: false,
+      setDontShowAgain: v => set({ dontShowAgain: v }),
+    }),
+    {
+      name: STORAGE_KEY,
+      storage: createJSONStorage(() => localStorage),
+      partialize: state => ({ dontShowAgain: state.dontShowAgain }),
     }
-  })(),
-
-  setDontShowAgain: v => {
-    try {
-      localStorage.setItem(STORAGE_KEY, v ? 'true' : 'false');
-    } catch (e) {
-      // ignore
-    }
-    set({ dontShowAgain: v });
-  },
-}));
+  )
+);
 
 export default useFeedbackStore;
