3119e2e74e37dd92758bebe04501a3f417fc2fb3
INSANE-P <chanbin0626@gmail.com>
2026-03-11 21:25:17 +0900

    fix: MainLayout 간격 복원

--- packages/admin/src/layouts/MainLayout.tsx
+++ packages/admin/src/layouts/MainLayout.tsx
@@ -26,7 +26,7 @@ function MainLayout() {
         <SideNavBar />
         <div className="flex-1 flex flex-col min-h-0">
           <Header />
-          <div className="flex-1 overflow-auto p-6">
+          <div className="flex-1 overflow-auto p-6 space-y-6">
             <Outlet />
           </div>
         </div>
