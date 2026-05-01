523c3e34e5d27d7e65283497817a6b9ac6bb4413
hyunwoo0081 <hyunwoo0081@gmail.com>
2026-05-01 22:12:55 +0900

    refactor: main banner allcll ui 로 변경

--- packages/client/src/widgets/home/ui/MainBanner.tsx
+++ packages/client/src/widgets/home/ui/MainBanner.tsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, SupportingText } from '@allcll/allcll-ui';
+import { Button, SupportingText, Heading, Flex } from '@allcll/allcll-ui';
 import useServiceSemester from '@/entities/semester/model/useServiceSemester';
 import { useSpringConfetti } from '@/widgets/home/lib/useMainBannerSeasonConfetti';
 import Section from '@/widgets/home/ui/Section.tsx';
@@ -20,41 +20,46 @@ function MainBanner() {
     <div className="relative overflow-hidden">
       <canvas ref={canvasRef} className="absolute inset-0 w-full h-full pointer-events-none z-10" />
       <Section
-        className="relative z-20 flex flex-col md:flex-row items-center justify-between !py-5"
+        className="relative z-20 flex flex-col md:flex-row items-center justify-between py-5!"
         bgColor="bg-banner-skysoft"
       >
         <div className="max-w-xl">
-          <div className="flex flex-row gap-2 items-center">
+          <Flex direction="flex-row" gap="gap-2" align="items-center">
             <Image src="/calendar.png" alt="2026-1학기 세종대 수강신청 일정 아이콘" className="w-10 h-10" />
             <span className="italic text-xs text-stone-500 ">
               {data?.semesterValue}학기 수강 정정 기간 <br />
               {START_DATE} ~ {END_DATE}
             </span>
-          </div>
+          </Flex>
 
-          <h1 className="text-lg mt-10 sm:text-xl md:text-2xl leading-snug font-bold mb-3">
+          <Heading level={1} className="mt-10 mb-3 text-lg! sm:text-xl! md:text-2xl! leading-snug font-bold">
             세종대 수강신청 연습 · 수강여석 확인 · 졸업요건까지
-          </h1>
-          <div className="flex flex-row justify-start items-center text-xl sm:text-2xl md:text-4xl leading-snug font-bold mb-3">
+          </Heading>
+          <Flex
+            direction="flex-row"
+            justify="justify-start"
+            align="items-center"
+            className="text-xl sm:text-2xl md:text-4xl leading-snug font-bold mb-3"
+          >
             <LogoName
               aria-label="올클(ALLCLL) 세종대 수강신청 도우미 서비스"
               className="w-20 sm:w-32 md:w-40 lg:w-48 mr-2 animate-updown"
             />
             이 도와드립니다!
-          </div>
+          </Flex>
           <SupportingText>
             수강 신청 연습, 실시간 여석 확인, 졸업요건 분석까지 <br className="md:hidden" />
             올클이 여러분의 학교생활과 함께합니다.
           </SupportingText>
 
-          <div className="flex flex-col md:flex-row gap-4 mt-4">
+          <Flex direction="flex-col" className="md:flex-row mt-4" gap="gap-4">
             <Button variant="primary" size="medium" asChild>
               <Link to="/live">전체 학년 여석 확인하기</Link>
             </Button>
             <Button variant="outlined" size="medium" asChild>
               <Link to="/simulation">수강 신청 연습하기</Link>
             </Button>
-          </div>
+          </Flex>
         </div>
         <Image
           src="/spring-banner-min.png"
