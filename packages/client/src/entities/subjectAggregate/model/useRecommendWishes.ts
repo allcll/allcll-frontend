f341cd2cbb24804b602a441ac50e52e1260fd98c
Hyunwoo Chae <76638365+hyunwoo0081@users.noreply.github.com>
2026-07-29 20:40:50 +0900

    fix: 관심과목 분석 상세 페이지 버그 수정 (#348)
    * feat: semester 관리 hook 추가
    
    * refactor: wishes detail page 코드 분리
    
    * fix: console.log 제거
    
    Remove console log for response status in error handling.
    
    * refactor: fsd layer 위반 코드 제거 (type WishesInfo shared 로 이동)
    
    * refactor: allcll ui 적용
    
    * refactor: semester banner 파일 분리
    
    * refactor: IWishesInfo 이름 변경, allcll-ui 적용, import type 추가, import tsx ts 확장자 제거
    
    * refactor: DOUGHNUT_SELECT_TYPES enum 에서 const object 로 변경
    
    * fix: reuse promise stream
    
    Co-authored-by: 박찬빈 <chanbin0626@gmail.com>
    
    * fix: jsonParse 반환 값을 명시할 수 있도록 변경 / 분리
    
    * feat: API Exception 타입 정의
    
    * fix: Error 처리 타입 명시적 선언, 옵셔널 체이닝 적용
    
    * fix: 학기 변경으로 인한 데이터 변경
    
    * fix: isPastSemester 제거
    
    * fix: type 컨벤션 적용
    
    * fix: import 에 확장자 제거
    
    ---------
    
    Co-authored-by: 박찬빈 <chanbin0626@gmail.com>

--- packages/client/src/entities/subjectAggregate/model/useRecommendWishes.ts
+++ packages/client/src/entities/subjectAggregate/model/useRecommendWishes.ts
@@ -1,19 +1,20 @@
+import type { IWishesInfo } from '@/shared/model/types';
 import useWishes, { InitWishes } from '@/entities/wishes/model/useWishes.ts';
 import { useJoinPreSeats } from '@/entities/subjectAggregate/lib/joinSubjects.ts';
 import useDetailWishes from '@/entities/subjectAggregate/model/useDetailWishes.ts';
 
-// Todo: Subject 부분은 따로 분리하기
 /** subjectId 에 대한 추천 과목을 반환합니다. */
-function useRecommendWishes(subjectId: number) {
-  const { data: wishes } = useWishes();
-  const { data: wish } = useDetailWishes(subjectId);
+function useRecommendWishes(wishesInfo: IWishesInfo) {
+  const { data: wishes } = useWishes(wishesInfo.semesterCode);
   const data = useJoinPreSeats(wishes, InitWishes);
 
+  // fixme: subjectCode 를 구하기 위한 용도.
+  const { data: wish } = useDetailWishes(wishesInfo);
   const subjectCode = wish?.subjectCode ?? '';
 
   if (!data) return { isPending: true };
 
-  const detail = data.filter(basket => basket.subjectCode === subjectCode && basket.subjectId !== subjectId);
+  const detail = data.filter(basket => basket.subjectCode === subjectCode && basket.subjectId !== wishesInfo.subjectId);
 
   return {
     isPending: false,
